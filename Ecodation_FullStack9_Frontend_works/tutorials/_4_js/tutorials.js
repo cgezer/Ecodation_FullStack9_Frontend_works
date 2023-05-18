3687aa252d081f5896d3e77e3f0f28aa0ee75fc9
cgezer <celalgezer@gmail.com>
2023-05-18 15:56:42 +0300

    temel js

--- Ecodation_FullStack9_Frontend_works/tutorials/_4_js/tutorials.js
+++ Ecodation_FullStack9_Frontend_works/tutorials/_4_js/tutorials.js
@@ -1,1 +1,21 @@
-alert("alert denemesi")
+alert("alert denemesi");
+console.log("consol dene;mesi")
+console.error("consol eror denemesi");
+console.warn("warn denemesi");
+console.info("info denemesi");
+alert("Pop-up");
+window.alert("Pop-up denemesi window");
+document.writeln("<br />ekran çıktısı için Document.write");
+document.writeln(" `` back ticks");
+var numberdata=33;
+var numberdata=3;
+let numberdata1=13;
+
+const numberdata2=23;
+numberdata1=21;
+console.log(numberdata1);
+var sayı=numberdata;
+let data=true;
+let datastr="selam";
+console.log(typeof data);
+console.log(typeof datastr);
