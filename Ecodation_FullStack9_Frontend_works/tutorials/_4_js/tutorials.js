a85bc72e269372538a821828d33baffe5c08d4b3
cgezer <celalgezer@gmail.com>
2023-05-19 12:16:33 +0300

    cast,promt,typeof,instanceof,operators

--- Ecodation_FullStack9_Frontend_works/tutorials/_4_js/tutorials.js
+++ Ecodation_FullStack9_Frontend_works/tutorials/_4_js/tutorials.js
@@ -5,7 +5,7 @@ console.warn("warn denemesi");
 console.info("info denemesi");
 alert("Pop-up");
 window.alert("Pop-up denemesi window");
-document.writeln("<br />ekran çıktısı için Document.write");
+document.writeln("<br/>"+"ekran çıktısı için Document.write");
 document.writeln(" `` back ticks");
 var numberdata=33;
 var numberdata=3;
@@ -19,3 +19,24 @@ let data=true;
 let datastr="selam";
 console.log(typeof data);
 console.log(typeof datastr);
+
+let num1=12, num2=32;
+
+console.log("num1+num2="+num1+num2);
+console.log("num1+num2="+(num1+num2));
+
+let deneme=["Ankara","Adıyaman","Malatya","Mersin"];
+console.log(deneme instanceof Array);
+console.log(deneme instanceof Object);
+console.log(deneme instanceof Number);
+console.log(deneme instanceof String);
+let userData=prompt("Lütfen birşeyler yazınız...");
+console.log(userData);
+document.writeln("<br/>"+userData);
+alert(userData);
+let typeCast=String(12089);
+console.log("type is:"+typeof typeCast);
+let example=Number(prompt("lütfen bir sayı giriniz."));
+console.log("example:"+example);
+let numberSqr=Number(prompt("Hangi sayının kare kökünü öğrenmek istiyorsunuz..."));
+alert(numberSqr+"'un karekökü="+Math.sqrt(numberSqr));
